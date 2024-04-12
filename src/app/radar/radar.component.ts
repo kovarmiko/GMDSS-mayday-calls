13ca19c1b521d84c93c829c78473e4840b0bc5e3
kovarmiko <kovarmiko@yahoo.com>
2024-04-12 09:56:14 +0200

    remove logic to connect radar as boat, handle unsubscribing

--- src/app/radar/radar.component.ts
+++ src/app/radar/radar.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { MessageService } from '../core/services/message.service';
 import { Client, ClientsMessage, ConnectionMessage } from '../types/types';
 import { takeUntil } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'safety-radar',
@@ -9,42 +10,49 @@ import { takeUntil } from 'rxjs/operators';
   styleUrls: ['./radar.component.scss'],
   providers: [MessageService],
 })
-export class RadarComponent
-  implements OnInit
-{
+export class RadarComponent implements OnInit, OnDestroy {
   @Input() set name(info: { deviceName: string; callSign: string }) {
     this.callSign = info.callSign;
     this.deviceName = info.deviceName;
   }
+
+  private sub = new Subscription();
 
   boatsInRange: Client[] = [];
 
   deviceName = '';
   callSign = '';
 
-  constructor(private messageService: MessageService) {
-  }
+  constructor(private messageService: MessageService) {}
 
   ngOnInit() {
-    this.messageService.ready$.subscribe(() => {
-      this.messageService.sendMessage(this.createConnectionMessage('connect'));
-    });
+    // keep this logic for future need
+    this.sub.add(
+      this.messageService.ready$.subscribe(() => {
+      
+      })
+    );
 
-    this.messageService.closed$.subscribe(() => {
-      this.messageService.sendMessage(
-        this.createConnectionMessage('disconnect')
-      );
-    });
+    this.sub.add(
+      this.messageService.closed$.subscribe(() => {
+        this.messageService.sendMessage(
+          this.createConnectionMessage('disconnect')
+        );
+      })
+    );
 
     const { clients$ } = this.messageService.connect('ws://localhost:8080');
-    clients$
-    .pipe(takeUntil(this.messageService.closed$))
-    .subscribe({
-      next: (m) => m && this.updateBoats(m),
-      error: (e: unknown) => console.error(e),
-      complete: () => console.info('completed clients transmission'),
-    });
+    this.sub.add(
+      clients$.pipe(takeUntil(this.messageService.closed$)).subscribe({
+        next: (m) => m && this.updateBoats(m),
+        error: (e: unknown) => console.error(e),
+        complete: () => console.info('completed clients transmission'),
+      })
+    );
+  }
 
+  ngOnDestroy(): void {
+      this.sub.unsubscribe()
   }
 
   private updateBoats(message: ClientsMessage) {
